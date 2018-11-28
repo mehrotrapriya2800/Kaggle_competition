11f762a7fe064500b65fd4beef640948fe8f554d
mehrotrapriya2800 <43611498+mehrotrapriya2800@users.noreply.github.com>
2018-11-28 15:32:28 +0800

    Update kaggle_code_github.py

--- kaggle_code_github.py
+++ kaggle_code_github.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import tensorflow as tf
-
-                            
+                        
 data_train_final = pd.read_csv("train.csv")                                
 test_data = pd.read_csv("test.csv")
 
@@ -17,25 +16,37 @@ data_train_final.loc[data_train_final['n_jobs'] == -1, 'n_jobs'] = 16
 data_train_final['new_col2'] = (data_train_final['max_iter'] * data_train_final['n_samples'])/data_train_final['n_jobs']
 
 lab = data_train_final["time"]
+
+
 data_train_final = data_train_final.drop(['time'], axis=1)
 pen = data_train_final["penalty"]
 pen1 = test_data["penalty"]
+
+
 numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 new_df_train = data_train_final.select_dtypes(include=numerics)
 new_df_test = test_data.select_dtypes(include=numerics)
+
 train = (new_df_train - new_df_test.mean())/new_df_test.std(ddof=0)
-train = train.join(pen)
+train = train.join(pen) #joining penalty column
 test = (new_df_test - new_df_test.mean())/new_df_test.std(ddof=0)
-test = test.join(pen1)
+test = test.join(pen1) #joining penalty column
+
+#dropping some columns
 
 train = train.drop(columns=['l1_ratio','scale','random_state','alpha','flip_y'])
 test = test.drop(columns=['l1_ratio','scale','random_state','alpha','flip_y'])
 
+#deciding the BATCH_SIZE and num_epochs
 
 BATCH_SIZE = 140
 num_epochs = 1000
+
+
 #converting train and test data to tensors
 input_train = tf.estimator.inputs.pandas_input_fn(x=train,y=lab,batch_size=BATCH_SIZE,num_epochs=num_epochs,shuffle=True)
+
+
 #converting our columns to tensorflow columns
 max_iter = tf.feature_column.numeric_column("max_iter")
 n_jobs = tf.feature_column.numeric_column("n_jobs")
@@ -63,19 +74,7 @@ Feature_columns = [
     tf.feature_column.indicator_column(penalty),
 ]
 
-wide_columns= [  
-    new_col1,
-    new_col2,
-    new_col3,
-    max_iter,
-    n_jobs, 
-    n_samples, 
-    n_features, 
-    n_classes,
-    n_clusters_per_class, 
-    n_informative,
-    
-]
+wide_columns= [new_col1, new_col2, new_col3, max_iter, n_jobs, n_samples, n_features, n_classes, n_clusters_per_class, n_informative,]
 
 model = tf.estimator.DNNLinearCombinedRegressor(\
     linear_feature_columns =wide_columns,\
@@ -92,9 +91,10 @@ predict_input_fn = tf.estimator.inputs.pandas_input_fn(\
         shuffle=False)
 
 predictions = model.predict(input_fn=predict_input_fn)
+
 prediction = []
-for i in predictions:
-    prediction.append(i["predictions"][0])
+for pred in predictions:
+    prediction.append(pred["predictions"][0])
 
 prediction = pd.DataFrame(prediction, columns=['time']).abs()
-prediction.to_csv('output_github_submit.csv', index=True, index_label='id')
+prediction.to_csv('output_github_submit.csv', index=True, index_label='id') #creating output file with predictions
